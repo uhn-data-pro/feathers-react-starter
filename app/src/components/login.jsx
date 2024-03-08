2eae88aa9fdc6cbd06db63f8a5559651dcf16030
mindychxn <mindychxn@gmail.com>
2024-03-08 10:01:53 -0500

    Convert login from class to function component

--- app/src/components/login.jsx
+++ app/src/components/login.jsx
@@ -1,63 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 
 import responsive from 'FRS/components/responsive.jsx';
 
-@responsive
-export default class Login extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			email: '',
-			password: '',
-		};
-	}
+export default function Login(props) {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-	handleChange = (field, value) => this.setState({ [field]: value });
-
-	render() {
-		const { authenticate } = this.props;
-		const { email, password } = this.state;
-
-		return (
-			<div style={{ padding: '0 20px' }}>
-				<TextField
-					fullWidth
-					required
-					id='email-local'
-					label='Email'
-					margin='normal'
-					onChange={(event) => this.handleChange('email', event.target.value)}
-					type='email'
-					variant='standard'
-					value={email}
-				/>
-				<TextField
-					fullWidth
-					required
-					id='password-local'
-					label='Password'
-					margin='normal'
-					onChange={(event) =>
-						this.handleChange('password', event.target.value)
-					}
-					type='password'
-					variant='outlined'
-					value={password}
-				/>
-				<div style={{ textAlign: 'center', marginBottom: 20, marginTop: 16 }}>
-					<Button
-						variant='contained'
-						color='secondary'
-						onClick={() => authenticate({ email, password })}
-						style={{ width: '100%' }}
-					>
-						Login
-					</Button>
-				</div>
+	return (
+		<div style={{ padding: '0 20px' }}>
+			<TextField
+				fullWidth
+				required
+				id='email-local'
+				label='Email'
+				margin='normal'
+				onChange={(event) => setEmail(event.target.value)}
+				type='email'
+				variant='outlined'
+				value={email}
+			/>
+			<TextField
+				fullWidth
+				required
+				id='password-local'
+				label='Password'
+				margin='normal'
+				onChange={(event) => setPassword(event.target.value)}
+				type='password'
+				variant='outlined'
+				value={password}
+			/>
+			<div style={{ textAlign: 'center', marginBottom: 20, marginTop: 16 }}>
+				<Button
+					variant='contained'
+					color='secondary'
+					onClick={() => props.authenticate({ email, password })}
+					style={{ width: '100%' }}
+				>
+					Login
+				</Button>
 			</div>
-		);
-	}
+		</div>
+	);
 }
