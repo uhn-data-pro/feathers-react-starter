b7d45151442658a9485b321c4b3dd2f07fc9d708
mindychxn <mindychxn@gmail.com>
2024-03-08 10:44:47 -0500

    Change registration from class to function component

--- app/src/components/registration.jsx
+++ app/src/components/registration.jsx
@@ -1,127 +1,105 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Snackbar from '@mui/material/Snackbar';
 
 import app from 'FRS/feathers-client.js';
-import responsive from 'FRS/components/responsive.jsx';
 
-@responsive
-export default class Registration extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			email: '',
-			password: '',
-			passwordConfirmation: '',
-			error: null,
-			snackBarOpen: false,
-			snackBarMessage: null,
-		};
-	}
+export default function Registration(props) {
+	const [newUser, setnewUser] = useState({
+		email: '',
+		password: '',
+		passwordConfirmation: '',
+	});
+	const [error, setError] = useState(null);
+	const [snackBarOpen, setSnackBarOpen] = useState(false);
+	const [snackBarMessage, setSnackBarMessage] = useState('');
 
-	handleCloseSnackBar = () => this.setState({ snackBarOpen: false });
+	const handleCloseSnackBar = () => setSnackBarOpen(false);
 
-	handleRegistrationChange = (field, value) =>
-		this.setState({ [field]: value, error: null });
+	const handleRegistrationChange = (field, value) =>
+		setnewUser({ ...newUser, [field]: value });
 
-	handleRegisterUser = (event) => {
+	const handleRegisterUser = (event) => {
 		event.preventDefault();
-		const { authenticate } = this.props;
-		const { email, password, passwordConfirmation } = this.state;
+		const { email, password } = newUser;
 
-		if (password !== passwordConfirmation) {
-			return this.setState({ error: 'Please make sure your passwords match' });
+		if (password !== newUser.passwordConfirmation) {
+			return setError('Please make sure your passwords match');
 		}
 
 		app
 			.service('users')
 			.create({ email, password })
-			.then(() => authenticate({ strategy: 'local', email, password }))
-			.catch(() =>
-				this.setState({
-					snackBarOpen: true,
-					snackBarMessage: 'Sorry, this email has already been used',
-				})
-			);
+			.then(() => props.authenticate({ strategy: 'local', email, password }))
+			.catch(() => {
+				setSnackBarOpen(true);
+				setSnackBarMessage('Sorry, this email has already been used');
+			});
 	};
 
-	render() {
-		const {
-			email,
-			password,
-			passwordConfirmation,
-			error,
-			snackBarOpen,
-			snackBarMessage,
-		} = this.state;
-
-		return (
-			<div style={{ padding: '0 20px' }}>
-				<Snackbar
-					anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-					open={snackBarOpen}
-					autoHideDuration={6000}
-					onClose={this.handleCloseSnackBar}
-					message={snackBarMessage}
-				/>
-				<TextField
-					error={error === 'email'}
-					fullWidth
-					required
-					id='email'
-					label='Email'
-					margin='normal'
-					onChange={(event) =>
-						this.handleRegistrationChange('email', event.target.value)
-					}
-					type='email'
-					variant='outlined'
-					value={email}
-				/>
-				<TextField
-					fullWidth
-					required
-					id='password'
-					label='Password'
-					margin='normal'
-					onChange={(event) =>
-						this.handleRegistrationChange('password', event.target.value)
-					}
-					type='password'
-					variant='outlined'
-					value={password}
-				/>
-				<TextField
-					error={!!error}
-					fullWidth
-					helperText={error}
-					required
-					id='password-confirmation'
-					label='Confirm Password'
-					margin='normal'
-					onChange={(event) =>
-						this.handleRegistrationChange(
-							'passwordConfirmation',
-							event.target.value
-						)
-					}
-					type='password'
-					variant='outlined'
-					value={passwordConfirmation}
-				/>
-				<div style={{ textAlign: 'center', marginBottom: 20, marginTop: 16 }}>
-					<Button
-						variant='contained'
-						color='secondary'
-						onClick={this.handleRegisterUser}
-						style={{ width: '100%' }}
-					>
-						Sign Up
-					</Button>
-				</div>
+	return (
+		<div style={{ padding: '0 20px' }}>
+			<Snackbar
+				anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+				open={snackBarOpen}
+				autoHideDuration={6000}
+				onClose={handleCloseSnackBar}
+				message={snackBarMessage}
+			/>
+			<TextField
+				error={error === 'email'}
+				fullWidth
+				required
+				id='email'
+				label='Email'
+				margin='normal'
+				onChange={(event) =>
+					handleRegistrationChange('email', event.target.value)
+				}
+				type='email'
+				variant='outlined'
+				value={newUser.email}
+			/>
+			<TextField
+				fullWidth
+				required
+				id='password'
+				label='Password'
+				margin='normal'
+				onChange={(event) =>
+					handleRegistrationChange('password', event.target.value)
+				}
+				type='password'
+				variant='outlined'
+				value={newUser.password}
+			/>
+			<TextField
+				error={!!error}
+				fullWidth
+				helperText={error}
+				required
+				id='password-confirmation'
+				label='Confirm Password'
+				margin='normal'
+				onChange={(event) =>
+					handleRegistrationChange('passwordConfirmation', event.target.value)
+				}
+				type='password'
+				variant='outlined'
+				value={newUser.passwordConfirmation}
+			/>
+			<div style={{ textAlign: 'center', marginBottom: 20, marginTop: 16 }}>
+				<Button
+					variant='contained'
+					color='secondary'
+					onClick={handleRegisterUser}
+					style={{ width: '100%' }}
+				>
+					Sign Up
+				</Button>
 			</div>
-		);
-	}
+		</div>
+	);
 }
