17ad74c302188cdd5e5bd1fbf69db5497a7b25ec
mindychxn <mindychxn@gmail.com>
2024-03-08 16:57:59 -0500

    Convert client to typescript

--- app/src/client.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-
-import { ThemeProvider } from '@mui/material/styles';
-import App from './App.jsx';
-
-ReactDOM.render(<App />, document.getElementById('app'));
