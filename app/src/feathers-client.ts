0a0e2b5ad7067564500cdab8d1c8db301c0966e4
mindychxn <mindychxn@gmail.com>
2024-03-11 10:55:38 -0400

    Convert feathers-client file to typescript

---
+++ app/src/feathers-client.ts
@@ -0,0 +1,13 @@
+import feathers from '@feathersjs/client';
+import rest from '@feathersjs/rest-client';
+import fetch from 'isomorphic-fetch';
+
+import { BASE_URL } from './constants';
+
+const app: any = feathers();
+const api = rest(BASE_URL).fetch(fetch);
+
+app.configure(api);
+app.configure(feathers.authentication());
+
+export { app as default };
