614e812a0df21f2a9bbba5efca381510fc25a743
mindychxn <mindychxn@gmail.com>
2024-04-08 10:19:55 -0400

    Create new mfa auth strategy, update login component

--- api/src/authentication.js
+++ api/src/authentication.js
@@ -1,13 +1,50 @@
-const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
-const { LocalStrategy } = require('@feathersjs/authentication-local');
-const { expressOauth } = require('@feathersjs/authentication-oauth');
+const {
+  AuthenticationService,
+  JWTStrategy,
+} = require("@feathersjs/authentication");
+const { LocalStrategy } = require("@feathersjs/authentication-local");
+const { expressOauth } = require("@feathersjs/authentication-oauth");
+const { NotAuthenticated } = require("@feathersjs/errors");
+const { authenticator } = require("otplib");
 
-module.exports = app => {
+module.exports = (app) => {
   const authentication = new AuthenticationService(app);
 
-  authentication.register('jwt', new JWTStrategy());
-  authentication.register('local', new LocalStrategy());
+  class AuthenticatorStrategy extends LocalStrategy {
+    async authenticate(authentication, params) {
+      //const { entity } = this.configuration;
+      const { email, password, otp } = authentication;
 
-  app.use('/authentication', authentication);
+      const result = await this.findEntity(email, params);
+      // verify password, throws error or returns entity
+      const user = await this.comparePassword(result, password);
+
+      // if user does not have 2FA enabled then authenticate them
+      if (!user.twoFa) {
+        return {
+          authentication: { strategy: this.name },
+          user: await this.getEntity(result, params),
+        };
+      }
+
+      // if otp is not sent along with the request and 2FA is enabled prompt for otp
+      if (!otp) {
+        throw new NotAuthenticated("Prompt for OTP");
+      } else {
+        const isValid = authenticator.check(otp, user.secretKey);
+        if (!isValid) throw new NotAuthenticated("Incorrect OTP");
+        return {
+          authentication: { strategy: this.name },
+          user: await this.getEntity(result, params),
+        };
+      }
+    }
+  }
+
+  authentication.register("jwt", new JWTStrategy());
+  authentication.register("mfa", new AuthenticatorStrategy());
+  authentication.register("local", new LocalStrategy());
+
+  app.use("/authentication", authentication);
   app.configure(expressOauth());
 };
